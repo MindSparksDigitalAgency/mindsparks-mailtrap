4ccfdb000d98b55a7d3ca7b513ecb53a58641070
codesyariah122 <pujiermanto@gmail.com>
2025-01-29 02:09:21 +0700

    update/index-headers

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
  * @expectedException
  */
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use App\config\Router;
@@ -16,11 +20,6 @@ use Monolog\Handler\StreamHandler;
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-
 
 $logger = new Logger('my_logger');
 
