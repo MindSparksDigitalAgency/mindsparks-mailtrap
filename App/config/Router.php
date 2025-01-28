55124bd794bc485f103cecf84b92bf13e0322fa0
codesyariah122 <pujiermanto@gmail.com>
2025-01-29 02:18:09 +0700

    fix/headers

--- App/config/Router.php
+++ App/config/Router.php
@@ -11,6 +11,13 @@ class Router
 
         $data = json_decode(file_get_contents("php://input"), true);
 
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            header("Access-Control-Allow-Origin: *");
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+            exit;
+        }
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header("Content-Type: application/json");
             header("Accept: application/json");
